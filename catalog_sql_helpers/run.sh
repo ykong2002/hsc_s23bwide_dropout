3debbc9495d265e036bb45f41e100d71e47aa43b
Youwen Kong <36063453+ykong2002@users.noreply.github.com>
2025-11-06 14:34:06 +0900

    Update run.sh

--- catalog_sql_helpers/run.sh
+++ catalog_sql_helpers/run.sh
@@ -1,26 +1,5 @@
-for f in sql_files_s23b/*.sql; do
-  base=$(basename "$f" .sql)
-  echo "Running $f..."
-  python3 hscSspQuery3.py --user kongyouwen --release-version dr4 --format fits "$f"
-done
-
-
-python3 run_sqls.py --user kongyouwen --release-version dr4 --format fits --sql-dir sql_files/
-
-export HSC_SSP_CAS_PASSWORD='Kyw200206045221@'
-
-export HSC_SSP_CAS_PASSWORD='Kyw200206045221@'
-
-for f in sql_files/2*.sql; do
-  base=$(basename "$f" .sql)
-  echo "Running $f..."
-  python3 hscSspQuery3.py --user kongyouwen --release-version dr4 --format fits "$f"
-done
-
-export LANG=en_US.UTF-8
-export LC_ALL=en_US.UTF-8
-export HSC_SSP_CAS_PASSWORD='Kyw200206045221@'
-for f in /home/kongyw/hsc_wide_dropout_2510/vlass/sqls/*.sql; do
+export HSC_SSP_CAS_PASSWORD={your stars account password}
+for f in /your/path/to/sqls/*.sql; do
   base=$(basename "$f" .sql)
   echo "Running $f..."
   python3 hscSspQuery3.py --user kongyouwen --release-version dr4 --format fits "$f"
